971492f17de4dc8437db993dc0fbf4abf68ce5d8
Pernelle Mensah <5522063+frenzymilk@users.noreply.github.com>
2025-11-07 08:58:22 -0500

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -16,44 +16,12 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role = aws_iam_role.lambda_execution_role.name
 }
-/*
-data "aws_iam_policy_document" "ts_lambda_policy" {
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = [
-      aws_cloudwatch_log_group.ts_lambda_loggroup.arn,
-      "${aws_cloudwatch_log_group.ts_lambda_loggroup.arn}:*"
-    ]
-  }
-}*/
 
 /*IAM role for Lambda execution*/
 resource "aws_iam_role" "lambda_execution_role" {
   name               = "lambda_execution_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
-/*
-resource "aws_cloudwatch_log_group" "ts_lambda_loggroup" {
-  name              = "/aws/lambda/${aws_lambda_function.counter_lambda.function_name}"
-  retention_in_days = 3
-}
-
-resource "aws_iam_role_policy" "ts_lambda_role_policy" {
-  policy = data.aws_iam_policy_document.ts_lambda_policy.json
-  role   = aws_iam_role.lambda_execution_role.id
-  name   = "my-lambda-policy"
-}*/
-/*
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id = "AllowExecutionFromAPIGateway"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.counter_lambda.function_name
-  principal = "apigateway.amazonaws.com"*/
-#  source_arn = "${aws_api_gateway_rest_api.visitors_api.execution_arn}/*/*/*"
-/*}*/
 
 # Package the Lambda function code
 data "archive_file" "counter_code" {
